9782ac0118021a5763610ecc34325c47192ec1cf
Zihao Zeng <zihao.zeng@student.manchester.ac.uk>
2025-06-03 02:40:24 +0100

    Fix the text not rendering problem

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import path from 'node:path'
 
 export default defineConfig({
   base: '/personal-portfolio/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VueI18nPlugin({
+      include: path.resolve(__dirname, './src/languages/**')
+    })
+  ]
 })
