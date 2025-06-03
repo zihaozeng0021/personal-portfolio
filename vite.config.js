e8658afc62fa22a8383eeb7e83e5976408da8824
Zihao Zeng <zihao.zeng@student.manchester.ac.uk>
2025-06-03 02:26:11 +0100

    Configure deployment but find some problem in language rendering

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/personal-portfolio/',
   plugins: [vue()],
 })
